c70278bfc9a03cb2c5516b8a53f1290877050231
petchsri666 <69156893+petchsri666@users.noreply.github.com>
2020-08-03 22:09:19 +0700

    Add files via upload

---
+++ compute_circle_area.py
@@ -0,0 +1,5 @@
+#Phittawat Champajan DME Y.2
+import math
+radius_from_user = float(input("Enter radius:"))
+area = math.pi*radius_from_user**2
+print("The area radius",radius_from_user,"is %.2f"%area)
